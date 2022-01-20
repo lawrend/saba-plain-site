9d2a94a4fd6c7d9e770f0acc1a97aef1e5a146a9
lawrend <douglas_lawrence@mac.com>
2022-01-19 17:15:53 -0800

    made header into component

--- components/header.js
+++ components/header.js
@@ -5,36 +5,16 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-
-    <!-- NAVBAR -->
-    <div>
-      <ul class="topnav -fixed" id="myTopnav">
-        <li>
-          <a id="top-name" class="brand" href="index.html">
-            Saba Omidvar</a>
-        </li>
-        <li>
-          <a href="about.html">About</a>
-        </li>
-
-        <li class="dropdown" id="treatmentDropdown">
-
-          <a href="javascript:void(0)" onclick="topnav('treatmentDropdown')" class="dropbtn">Treatments</a>
-          <div class="dropdown-content">
-            <a href="#">Adults</a>
-            <a href="#">Teens</a>
-            <a href="#">Couples</a>
-          </div>
-        </li>
-
-        <li style="float:right;"><a href="#" >Schedule an Appointment</a></li>
-        <li class="-icon">
-          <a href="javascript:void(0);" onclick="topnav('myTopnav')">☰</a>
-        </li>
-      </ul>
-
-    </div>
-
+    <!-- PARALLAX 1-->
+    <div class="bg-parallax twenty-view _alignCenter" style="background-image: url('assets/images/beach.jpg')">
+      <div class="-front front-reg shepp">
+          <h1>Saba Omidvar
+            <p>
+            <small class="mont">Therapy? Ew.</small>
+            </p>
+          </h1>
+      </div><!--front-->
+    </div><!--bg-parallax-->
     `;
   }
 }
---
+++ components/navbar.js
@@ -0,0 +1,42 @@
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+
+    <!-- NAVBAR -->
+    <div>
+      <ul class="topnav -fixed" id="myTopnav">
+        <li>
+          <a id="top-name" class="brand" href="index.html">
+            Saba Omidvar</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+
+        <li class="dropdown" id="treatmentDropdown">
+
+          <a href="javascript:void(0)" onclick="topnav('treatmentDropdown')" class="dropbtn">Treatments</a>
+          <div class="dropdown-content">
+            <a href="#">Adults</a>
+            <a href="#">Teens</a>
+            <a href="#">Couples</a>
+          </div>
+        </li>
+
+        <li style="float:right;"><a href="#" >Schedule an Appointment</a></li>
+        <li class="-icon">
+          <a href="javascript:void(0);" onclick="topnav('myTopnav')">☰</a>
+        </li>
+      </ul>
+
+    </div>
+
+    `;
+  }
+}
+
+customElements.define('navbar-component', Navbar);
