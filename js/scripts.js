5a62887b2a1878c4e399ab328c6c717ff6226f91
lawrend <douglas_lawrence@mac.com>
2022-01-22 17:26:59 -0800

    fade in and out on load and page view

--- js/scripts.js
+++ js/scripts.js
@@ -27,5 +27,68 @@ function callbackFunc() {
   }
 }
 
+
+// fade in out
+//
+//
+//
+////
+//$(window).on("load",function() {
+//    function fade() {
+//        var animation_height = $(window).innerHeight() * 0.25;
+//        var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+
+//        $('.fade').each(function() {
+
+//            var objectTop = $(this).offset().top;
+//            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+//            if ( objectTop < windowBottom ) {
+//                if ( objectTop < windowBottom - animation_height ) {
+//                    $(this).html( 'fully visible' );
+//                    $(this).css( {
+//                        transition: 'opacity 0.1s linear',
+//                        opacity: 1
+//                    } );
+
+//                } else {
+//                    $(this).html( 'fading in/out' );
+//                    $(this).css( {
+//                        transition: 'opacity 0.25s linear',
+//                        opacity: (windowBottom - objectTop) * ratio
+//                    } );
+//                }
+//            } else {
+//                $(this).html( 'not visible' );
+//                $(this).css( 'opacity', 0 );
+//            }
+//        });
+//    }
+//    $('.fade').css( 'opacity', 0 );
+//    fade();
+//    $(window).scroll(function() {fade();});
+
+// $(document).ready(function() {
+
+// /* Every time the window is scrolled ... */
+// $(window).scroll( function(){
+
+//     /* Check the location of each desired element */
+//     $('.hideme').each( function(i){
+
+//         var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//         var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+//         /* If the object is completely visible in the window, fade it it */
+//         if( bottom_of_window > bottom_of_object ){
+
+//             $(this).animate({'opacity':'1'},500);
+
+//         }
+//     });
+// });
+// });
+
+
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
